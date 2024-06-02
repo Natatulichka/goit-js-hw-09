816db37f4ba0dac59a2acdda0ca7fbe3be213d31
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-06-02 16:28:17 +0200

    add

---
+++ src/js/3-form.js
@@ -0,0 +1,53 @@
+// Пошук DOM-елементів та додавання класів
+const form = document.querySelector('.feedback-form');
+form.querySelector('textarea').classList.add('feedback-form-textarea');
+form.querySelector('input').classList.add('feedback-form-input');
+form.querySelector('button').classList.add('feedback-form-button');
+//  Прослуховувач подій при вводі данних та збереження до локального сховища.
+form.addEventListener('input', () => {
+  const formData = new FormData(form);
+  const email = formData.get('email').trim();
+  const message = formData.get('message').trim();
+  const data = { email, message };
+  saveToLS('feedback-form-state', data);
+});
+// Прослуховувач подій при натисканні на кнопку та виведення данних з локального сховища
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const email = formData.get('email').trim();
+  const message = formData.get('message').trim();
+  const data = { email, message };
+  if (!data.email || !data.message) {
+    alert('Fill please all fields');
+  } else {
+    console.log(data);
+    form.reset();
+
+    localStorage.removeItem('feedback-form-state');
+  }
+});
+// шаблонна функція для збереження у локальне сховище
+function saveToLS(key, value) {
+  const jsonData = JSON.stringify(value);
+  localStorage.setItem(key, jsonData);
+}
+// шаблонна функція для виведення данних з локального сховища
+function loadFromLS(key) {
+  const json = localStorage.getItem(key);
+  // Конструкція try...catch ловить тільки помилки, які виникли під час виконання коду (runtime errors).
+  try {
+    const data = JSON.parse(json);
+    return data;
+  } catch {
+    return json;
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  const data = loadFromLS('feedback-form-state');
+
+  form.elements.email.value = data?.email ?? '';
+  form.elements.message.value = data?.message ?? '';
+});
